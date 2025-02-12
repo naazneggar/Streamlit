bf7b5df338942de5c4a13ee0da12308d4b80c6de
Naseef <newseefgenesis@gmail.com>
2025-02-12 13:29:06 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -22,7 +22,7 @@ if page=="PerCapitaIncome":
     prediction=model1.predict([[year]])
     prediction=pd.Series(prediction[0])
     if st.button('Predict'):
-        st.success('The per capita income of canada in the year',year,'is',round(prediction[0],2))
+        st.success('The per capita income of canada in the year',year,'is',prediction[0])
 else:
     st.title('House Price Prediction')
     st.write('This is a simple web_app to predict the House price')
