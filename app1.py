f942c8a42f9ecc20418ba6028dae253e0839928b
Naseef <newseefgenesis@gmail.com>
2025-02-12 13:28:03 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -20,9 +20,9 @@ if page=="PerCapitaIncome":
 
     year=st.number_input('Enter the year',min_value=100)
     prediction=model1.predict([[year]])
-    prediction=pd.Series(prediction)
+    prediction=pd.Series(prediction[0])
     if st.button('Predict'):
-        st.success('The per capita income of canada in the year',year,'is',prediction[0])
+        st.success('The per capita income of canada in the year',year,'is',round(prediction[0],2))
 else:
     st.title('House Price Prediction')
     st.write('This is a simple web_app to predict the House price')
